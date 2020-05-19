e04291b21cfad1f328effb3d0a84e0a0cd49e722
seohoon-dev <sh931123@gmail.com>
2020-05-19 17:55:56 +0900

    [#1] 프로젝트 생성

---
+++ src/main/java/com/hoon/electronic/ElectronicApplication.java
@@ -0,0 +1,13 @@
+package com.hoon.electronic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectronicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectronicApplication.class, args);
+	}
+
+}
