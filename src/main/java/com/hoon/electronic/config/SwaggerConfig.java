a9f14d2a4bcf85ae0a15726eb16738b78806dfe6
seohoon-dev <sh931123@gmail.com>
2020-08-12 16:54:13 +0900

    [#26] swagger를 사용하여 API 문서 생성

---
+++ src/main/java/com/hoon/electronic/config/SwaggerConfig.java
@@ -0,0 +1,40 @@
+package com.hoon.electronic.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+
+@EnableSwagger2
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.hoon.electronic.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "Electronic Shopping Mall API",
+                "전자 물품 거래 서비스 API 문서",
+                "ver1",
+                "www.example.com",
+                new Contact("Contact Me", "www.example.com", "foo@example.com"),
+                "Licenses",
+                "www.example.com",
+                new ArrayList<>());
+    }
+}
